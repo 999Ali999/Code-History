12f6fa93c40748f96640b521057f6d7a64463971
JuiceWRLD <127804196+999Ali999@users.noreply.github.com>
2024-01-07 22:57:08 +0100

    init commit

---
+++ Code History/Code History/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Ali on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
