e5d209a3841842e09286da9e434040c5644681e4
JuiceWRLD <127804196+999Ali999@users.noreply.github.com>
2024-01-20 18:42:25 +0100

    refactoring the code

---
+++ Code History/Code History/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Ali on 20/01/24.
+//
+
+import Foundation
+
+struct Question {
+    var questionText: String
+    var possibleAnswers: [String]
+    var correctAnswerIndex: Int
+}
